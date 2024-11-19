4ba57b97573f8192f58144ff96e4e80f41c66343
cwj0417 <chenwenjun@yo-cwj.com>
2024-11-20 01:01:07 +0800

    feat: update sync script

--- scripts/updateDataFromAction.js
+++ scripts/updateDataFromAction.js
@@ -1,1 +1,8 @@
-console.log(process.argv)
+import fse from 'fs-extra'
+const date = process.argv[2]
+const detail = decodeURI(process.argv[3])
+let data = JSON.parse(fse.readFileSync('src/data.json'))
+if (!data[date]) {
+    data[date] = { detail }
+    fse.writeJSONSync('src/data.json', data)
+}
