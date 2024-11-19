b8b7edbdce763a5072b6acfb1c3be8ec29eb2eba
cwj0417 <chenwenjun@yo-cwj.com>
2024-11-20 03:13:54 +0800

    feat: add update2

--- scripts/updateDataFromAction.js
+++ scripts/updateDataFromAction.js
@@ -1,8 +1,14 @@
 import fse from 'fs-extra'
 const date = process.argv[2]
 const detail = decodeURI(process.argv[3])
+const hh = process.argv[4] || 0
+const s3 = process.argv[5] || 0
+const s4 = process.argv[6] || 0
 let data = JSON.parse(fse.readFileSync('src/data.json'))
 if (!data[date]) {
     data[date] = { detail }
+    if (hh) data[date].hh = hh
+    if (s3) data[date].s3 = s3
+    if (s4) data[date].s4 = s4
     fse.writeJSONSync('src/data.json', data)
 }
