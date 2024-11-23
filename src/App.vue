cd59ae52ccd75ec4109d7c22370394827db4e86b
cwj0417 <chenwenjun@yo-cwj.com>
2024-11-24 03:14:33 +0800

    chore: fix date start

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import data from './data.json'
 
 const datasource = Object.fromEntries(Object.entries(data).map(([k, v]) => ([new Date(k).valueOf(), v])))
 
-const dateRange = ['2024-01-29', '2024-12-30']
+const dateRange = ['2024-01-27', '2024-12-30']
 const days = (new Date(dateRange[1]) - new Date(dateRange[0])) / 86400000
 const firstDate = new Date(dateRange[0]).valueOf()
 
